9f2f88f56b5ed506c406e1cb7c365d04b54bf91d
Paul Vaden <pvaden@uw.edu>
2017-03-03 15:08:22 -0800

    added graph

--- server.R
+++ server.R
@@ -2,20 +2,21 @@ library(ggplot2)
 library(shiny)
 library(dplyr)
 library(tidyr)
-baby_names <- read.csv("data/baby-names.csv") %>% 
-              filter(name == "Ronald", sex == "boy")
+baby_names <- read.csv("data/baby-names.csv")
+presidents <- read.csv("data/presidents.csv")
 shinyServer(function(input, output, session) {
   
   
   filtered <- reactive({
-    data <- baby_names
+    data <- baby_names %>% filter(name == input$selection) 
     
     return(data)
   })
   
   output$plot <- renderPlot({
     p <- ggplot((data = filtered()), mapping = aes(x = year, y = percent)) +
-      geom_point() 
+      geom_point() +
+      geom_vline(xintercept = filter(presidents, first == input$selection)$year[1])
       
     
     return(p)
--- ui.R
+++ ui.R
@@ -5,13 +5,18 @@ library(dplyr)
 library(tidyr)
 
 # Read in the baby names data set
-baby.names <- read.csv("data/baby-names.csv")
+baby_names <- read.csv("data/baby-names.csv")
+presidents <- read.csv("data/presidents.csv")
 
 shinyUI(fluidPage(
   titlePanel(""),
   
   sidebarLayout(
-    sidebarPanel(),
+    sidebarPanel(
+      selectInput("selection", "Choose a president:",
+                  choices = presidents$first)
+      
+    ),
     mainPanel(
       plotOutput("plot", click = 'plot_click')
     )
