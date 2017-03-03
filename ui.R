7dae02178fb1604f2b7d8e2112c398644ebc862d
John Batts <battsj5@uw.edu>
2017-03-02 18:53:04 -0800

    Add ui.R and server.R and read in the dataset

---
+++ ui.R
@@ -0,0 +1,8 @@
+# Load the appropriate libraries
+library(ggplot2)
+library(shiny)
+library(dplyr)
+library(tidyr)
+
+# Read in the baby names data set
+baby.names <- read.csv("data/baby-names.csv")
