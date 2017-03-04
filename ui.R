df06c963812f03fef7aede2fa5b608b0420f7da3
Daniel Barnes <dbarnes2@uw.edu>
2017-03-03 22:29:25 -0800

    saves ui

--- ui.R
+++ ui.R
@@ -6,3 +6,10 @@ library(tidyr)
 
 # Read in the baby names data set
 baby.names <- read.csv("data/baby-names.csv")
+baby.names.1880 <- baby.names %>%
+  select(year = 1880)
+
+
+# Presidents
+# Time Person
+# Award winner
