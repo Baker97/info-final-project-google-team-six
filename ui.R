0ffbb078f53de8df553507d59521ea679eb12dc6
Andrew Baker <adbaker2@uw.edu>
2017-03-06 20:43:35 -0800

    adds a zoom function for the plot

--- ui.R
+++ ui.R
@@ -9,6 +9,7 @@ baby_names <- read.csv("data/baby-names.csv")
 presidents <- read.csv("data/presidents.csv")
 grammys <- read.csv("data/grammy.csv")
 authors <- read.csv("data/authors.csv", fileEncoding="UTF-8-BOM")
+colnames(authors) <- c("year", "first","full", "gender", "name")
 
 # UI
 shinyUI(fluidPage(
@@ -39,16 +40,14 @@ shinyUI(fluidPage(
     ),
     mainPanel(
       tabsetPanel(type = "tabs",
-        tabPanel(strong("Plot"), plotOutput("plot", click = 'plot_click'), 
-                 fluidRow(column(width = 5, verbatimTextOutput("click_info")))),
+        tabPanel(strong("Plot"), plotOutput("plot", click = 'plot_click', dblclick = "plot_dblclick", brush = brushOpts(
+                 id = "plot_brush", resetOnNew = TRUE)), fluidRow(column(width = 5, verbatimTextOutput("click_info")),
+                 column(width = 5,verbatimTextOutput("brush_info")))),
         
         tabPanel(strong("Table"), br(), p("This is a table of all the data points listed under the president's name"),
                  dataTableOutput("table")) 
+        )
       )
-        
-    )
-    
     )
   )
-  
 )
