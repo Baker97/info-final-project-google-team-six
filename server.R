c8e9bfbc176ee483b01110f75c6a0e0d55c36e39
Paul Vaden <pvaden@uw.edu>
2017-03-07 22:40:53 -0800

    no changes

--- server.R
+++ server.R
@@ -4,6 +4,7 @@ library(shiny)
 library(dplyr)
 library(tidyr)
 library(shinythemes)
+
 # Load in the datasets
 baby_names <- read.csv("data/baby-names.csv", stringsAsFactors = FALSE)
 presidents <- read.csv("data/presidents.csv", stringsAsFactors = FALSE)
@@ -11,59 +12,68 @@ grammys <- read.csv("data/grammy.csv", stringsAsFactors = FALSE)
 authors <- read.csv("data/authors.csv", fileEncoding="UTF-8-BOM", stringsAsFactors = FALSE)
 colnames(authors) <- c("year", "first","full", "gender", "name")
 
-
 # Server
 shinyServer(function(input, output, session) {
   
+  # Stores reactive values 
   values <- reactiveValues()
+  # Sets the default year to "2000"
   values$year <- 2000
   
+  # sets the range for plots?
   ranges <- reactiveValues(x = NULL, y = NULL)
   
-  filtered1 <- reactive({
-    data1 <- baby_names %>% filter(first == input$presselection, gender == "boy")
-    values$year <- filter(presidents, first == input$presselection)$year
-    return(data1)
-  })
-  filtered2 <- reactive({
-    data2 <- baby_names %>% filter(first == input$singerselection)
-    values$year <- filter(grammys, first == input$singerselection)$year
-    return(data2)
-  })
-  filtered3 <- reactive({
-    data3 <- baby_names %>% filter(first == input$authorselection) 
-    values$year <- filter(authors, first == input$authorselection)$year
-    
-    return(data3)
+  # Stores in "president_filtered" filtered data based upon "president_selection"
+  president_filtered <- reactive({
+    data1 <- baby_names %>% filter(first == input$president_selection, gender == "boy")
+    values$year <- filter(presidents, first == input$president_selection)$year
+    return(president_data)
   })
   
-  output$plot1 <- renderPlot({
-    p <- ggplot((data = filtered1()), mapping = aes(x = year, y = percent)) +
+  # Stores in "singer_filtered" filtered data based upon "singer_selection"
+  singer_filtered <- reactive({
+    data2 <- baby_names %>% filter(first == input$singer_selection)
+    values$year <- filter(grammys, first == input$singer_selection)$year
+    return(singer_data)
+  })
+  
+  # Stores in "author_filtered" filtered data based upon "author_selection"
+  author_filtered <- reactive({
+    author_data <- baby_names %>% filter(first == input$author_selection) 
+    values$year <- filter(authors, first == input$author_selection)$year
+    return(author_data)
+  })
+  
+  # Outputs to "president_plot" plot data for presidents
+  output$president_plot <- renderPlot({
+    p <- ggplot((data = president_filtereed()), mapping = aes(x = year, y = percent)) +
       geom_point() +
       geom_vline(xintercept = values$year) +
       coord_cartesian(xlim = ranges$x, ylim = ranges$y)
-    
-    
     return(p)
   })
-  output$plot2 <- renderPlot({
-    q <- ggplot((data = filtered2()), mapping = aes(x = year, y = percent)) +
+  
+  # Outputs to "singer_plot" plot data for singers
+  output$singer_plot <- renderPlot({
+    q <- ggplot((data = singer_filtered()), mapping = aes(x = year, y = percent)) +
       geom_point() +
       geom_vline(xintercept = values$year) +
       coord_cartesian(xlim = ranges$x, ylim = ranges$y)
-    
-    
     return(q)
   })
-  output$plot3 <- renderPlot({
-    w <- ggplot((data = filtered3()), mapping = aes(x = year, y = percent)) +
+  
+  # Outputs to "author_plot" plot data for authors
+  output$author_plot <- renderPlot({
+    w <- ggplot((data = author_filtered()), mapping = aes(x = year, y = percent)) +
       geom_point() +
       geom_vline(xintercept = values$year) +
       coord_cartesian(xlim = ranges$x, ylim = ranges$y)
-    
-    
     return(w)
   })
+  
+  # Not sure if we need separate clicks/brushes for each plot
+  
+  # observes event "plot_dblclick", and zooms in on range
   observeEvent(input$plot_dblclick, {
     brush <- input$plot_brush
     if (!is.null(brush)) {
@@ -76,22 +86,26 @@ shinyServer(function(input, output, session) {
     }
   })
   
+  # prints information about brushed area
   output$brush_info <- renderPrint({
     cat("input$plot_brush:\n")
     str(input$plot_brush)
   })
   
+  # prints information about plot click
   output$click_info <- renderPrint({
     cat("input$plot_click:\n")
     str(input$plot_click)
   })
   
-  output$table <- renderDataTable({
-    return(filtered1())
+  
+  # Renders data table for "president_table" with filtered president data
+  output$president_table <- renderDataTable({
+    return(president_filtered())
   })
   
-  #returns a summary of the filtered data
-  output$summary <- renderPrint({
-    summary(data.frame(filtered1()))
+  # Returns a summary of the filtered president data
+  output$president_summary <- renderPrint({
+    summary(data.frame(president_filtered()))
   })
 })
