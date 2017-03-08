d610b08d063d62eb1c890152d1b79519c84b31c5
Daniel Barnes <dbarnes2@uw.edu>
2017-03-07 16:18:46 -0800

    Adds navbar

--- ui.R
+++ ui.R
@@ -13,44 +13,147 @@ colnames(authors) <- c("year", "first","full", "gender", "name")
 
 # UI
 shinyUI(fluidPage(
+  # Title of page
   titlePanel("Baby names observation data"),
   br(),
   
-  sidebarLayout(
-    sidebarPanel(
-      h3(strong("Please click one box")),
-      p("graphs will only display one data set at a time"),
-      checkboxInput(inputId = "showpres",
-                    label = strong("Include President"),
-                    value = TRUE),
-      selectInput("presselection", "Choose a president:",
-                  choices = presidents$first),
-      checkboxInput(inputId = "showauth",
-                    label = strong("Include Author"),
-                    value = FALSE),
-      selectInput("authorselection", "Choose a Author:",
-                  choices = authors$first),
-      checkboxInput(inputId = "showsing",
-                    label = strong("Include Singer"),
-                    value = FALSE),
-      selectInput("singerselection", "Choose a singer:",
-                  choices = grammys$first)
-    ),
-    mainPanel(
-      tabsetPanel(type = "tabs",
-        tabPanel(strong("Plot"), plotOutput("plot", click = 'plot_click', dblclick = "plot_dblclick", brush = brushOpts(
-                 id = "plot_brush", resetOnNew = TRUE)), fluidRow(column(width = 5, verbatimTextOutput("click_info")),
-                 column(width = 5,verbatimTextOutput("brush_info")))),
+  # Nav bar, with nav bar title
+  navbarPage("Page Title",
+    
+    # President's page         
+    tabPanel("Presidents", 
+      sidebarLayout(
         
-        tabPanel(strong("Table"), br(), p("This is a table of all the data points listed under the user's selected name and profession"),
-                 dataTableOutput("table"), wellPanel(helpText(a("Baby names source data", href="http://www.worldbank.org/")), 
-                  helpText(a("Grammy data source", href = "http://www.worldbank.org/")), 
-                  helpText(a("President source data", href="http://www.worldbank.org/")))), 
+        # Presidents' user controls
+        sidebarPanel(
+          h3(strong("Please click one box")),
+          p("graphs will only display one data set at a time"),
+          checkboxInput(inputId = "showpres",
+                        label = strong("Include President"),
+                        value = TRUE),
+          selectInput("presselection", "Choose a president:",
+                      choices = presidents$first),
+          checkboxInput(inputId = "showauth",
+                        label = strong("Include Author"),
+                        value = FALSE),
+          selectInput("authorselection", "Choose a Author:",
+                      choices = authors$first),
+          checkboxInput(inputId = "showsing",
+                        label = strong("Include Singer"),
+                        value = FALSE),
+          selectInput("singerselection", "Choose a singer:",
+                      choices = grammys$first)
+        ),
         
-        tabPanel(strong("Summary"), br(), p("Shows a summary of the data selected (same as the data points 
-                                      displayed on the table table tab)"), verbatimTextOutput("summary"))
+        # President's data
+        mainPanel(
+          tabsetPanel(type = "tabs",
+            tabPanel(strong("Plot"), plotOutput("plot", click = 'plot_click', dblclick = "plot_dblclick", brush = brushOpts(
+                     id = "plot_brush", resetOnNew = TRUE)), fluidRow(column(width = 5, verbatimTextOutput("click_info")),
+                     column(width = 5,verbatimTextOutput("brush_info")))),
+            
+            tabPanel(strong("Table"), br(), p("This is a table of all the data points listed under the user's selected name and profession"),
+                     dataTableOutput("table"), wellPanel(helpText(a("Baby names source data", href="http://www.worldbank.org/")), 
+                      helpText(a("Grammy data source", href = "http://www.worldbank.org/")), 
+                      helpText(a("President source data", href="http://www.worldbank.org/")))), 
+            
+            tabPanel(strong("Summary"), br(), p("Shows a summary of the data selected (same as the data points 
+                                          displayed on the table table tab)"), verbatimTextOutput("summary"))
+            )
+          )
         )
-      )
-    )
+    ),
+    
+    # Musicians' page
+    tabPanel("Music",      
+             sidebarLayout(
+               
+               # Mucisians' user controls
+               sidebarPanel(
+                 h3(strong("Please click one box")),
+                 p("graphs will only display one data set at a time"),
+                 checkboxInput(inputId = "showpres",
+                               label = strong("Include President"),
+                               value = TRUE),
+                 selectInput("presselection", "Choose a president:",
+                             choices = presidents$first),
+                 checkboxInput(inputId = "showauth",
+                               label = strong("Include Author"),
+                               value = FALSE),
+                 selectInput("authorselection", "Choose a Author:",
+                             choices = authors$first),
+                 checkboxInput(inputId = "showsing",
+                               label = strong("Include Singer"),
+                               value = FALSE),
+                 selectInput("singerselection", "Choose a singer:",
+                             choices = grammys$first)
+               ),
+               
+               # Mucisians' data
+               mainPanel(
+                 tabsetPanel(type = "tabs",
+                             tabPanel(strong("Plot"), plotOutput("plot", click = 'plot_click', dblclick = "plot_dblclick", brush = brushOpts(
+                               id = "plot_brush", resetOnNew = TRUE)), fluidRow(column(width = 5, verbatimTextOutput("click_info")),
+                                                                                column(width = 5,verbatimTextOutput("brush_info")))),
+                             
+                             tabPanel(strong("Table"), br(), p("This is a table of all the data points listed under the user's selected name and profession"),
+                                      dataTableOutput("table"), wellPanel(helpText(a("Baby names source data", href="http://www.worldbank.org/")), 
+                                                                          helpText(a("Grammy data source", href = "http://www.worldbank.org/")), 
+                                                                          helpText(a("President source data", href="http://www.worldbank.org/")))), 
+                             
+                             tabPanel(strong("Summary"), br(), p("Shows a summary of the data selected (same as the data points 
+                                                                 displayed on the table table tab)"), verbatimTextOutput("summary"))
+                             )
+                 )
+               )
+             ),
+    
+    # Authors' data
+    tabPanel("Authors",      
+             sidebarLayout(
+               
+               # Authors' user controls
+               sidebarPanel(
+                 h3(strong("Please click one box")),
+                 p("graphs will only display one data set at a time"),
+                 checkboxInput(inputId = "showpres",
+                               label = strong("Include President"),
+                               value = TRUE),
+                 selectInput("presselection", "Choose a president:",
+                             choices = presidents$first),
+                 checkboxInput(inputId = "showauth",
+                               label = strong("Include Author"),
+                               value = FALSE),
+                 selectInput("authorselection", "Choose a Author:",
+                             choices = authors$first),
+                 checkboxInput(inputId = "showsing",
+                               label = strong("Include Singer"),
+                               value = FALSE),
+                 selectInput("singerselection", "Choose a singer:",
+                             choices = grammys$first)
+               ),
+               
+               # Authors' data
+               mainPanel(
+                 tabsetPanel(type = "tabs",
+                             tabPanel(strong("Plot"), plotOutput("plot", click = 'plot_click', dblclick = "plot_dblclick", brush = brushOpts(
+                               id = "plot_brush", resetOnNew = TRUE)), fluidRow(column(width = 5, verbatimTextOutput("click_info")),
+                                                                                column(width = 5,verbatimTextOutput("brush_info")))),
+                             
+                             tabPanel(strong("Table"), br(), p("This is a table of all the data points listed under the user's selected name and profession"),
+                                      dataTableOutput("table"), wellPanel(helpText(a("Baby names source data", href="http://www.worldbank.org/")), 
+                                                                          helpText(a("Grammy data source", href = "http://www.worldbank.org/")), 
+                                                                          helpText(a("President source data", href="http://www.worldbank.org/")))), 
+                             
+                             tabPanel(strong("Summary"), br(), p("Shows a summary of the data selected (same as the data points 
+                                                                 displayed on the table table tab)"), verbatimTextOutput("summary"))
+                             )
+                 )
+               )
+             ),
+    
+    # Data summary
+    tabPanel("Summary")
+  )
   )
 )
